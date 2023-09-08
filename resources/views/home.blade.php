3d6b1cdb66cae3f6ce019dff8618fb2d0bc49451
Jp Mendoza <jeanpauljr.mendoza@gmail.com>
2023-09-08 21:29:30 +0800

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To Do Webpage</title>
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+</head>
+<body>
+    <div class="centered-div">
+        <h1>JP'S TO DO LIST</h1><br>
+    </div>
+    <div class="centered-div">
+        <form action="">
+            <table>
+                <tr>
+                    <td><input type="text" name="" id="" placeholder="Input Task Here"></td>
+                    <td><button type="submit">Add Task</button></td>
+                </tr>
+                <tr>
+                    {{-- add tasks here?? --}}
+                </tr>
+            </table>
+        </form>
+    </div>
+</body>
+</html>
